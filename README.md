c24fab05f29c7594f14580b90144470b14ef88bf
Anna Lin <anna.lin@sfm24wf1g4l.na.akqa.local>
2015-11-17 15:57:18 -0800

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+1. rethinkdb
+2. npm start
+3. localhost:3000 (to view app)
+4. localhost:8080 (rethinkdb dashboard)
+5. r.db('links').table('Link') // to find the data
--- routes/api/link.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-let app = require('../../app');
-let Link = require('../../models/Link');
-
-module.exports = router;
